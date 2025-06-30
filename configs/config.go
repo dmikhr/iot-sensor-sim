c8ec3015fea0d0a76687454d12cd4faf4158015e
Dmitrii Khramtsov <dmitrypkh@gmail.com>
2025-06-30 16:45:34 +0300

    app config

---
+++ configs/config.go
@@ -0,0 +1,16 @@
+package config
+
+import "time"
+
+type Config struct {
+	MaxWorkers     int
+	Port           int
+	EmitterTimeout time.Duration
+	TargetAddress  string
+	TargetPort     int
+	TargetPath     string
+	// in seconds
+	SimDuration int
+	ExpCode     int
+	DryRun      bool
+}
