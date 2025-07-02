bab6aeb9be08d5b9128a6e5b638b141d9cfdbd21
Dmitrii Khramtsov <dmitrypkh@gmail.com>
2025-07-02 10:09:40 +0300

    helper funcs for working with time

---
+++ internal/timeutil/timeutil.go
@@ -0,0 +1,17 @@
+package timeutil
+
+import "time"
+
+// SecToDuration - convert seconds to time.Duration for timeout context
+func SecToDuration(seconds int) time.Duration {
+	return time.Duration(seconds) * time.Second
+}
+
+// HzToDuration - convert Hz to time.Duration
+func HzToDuration(hz float64) time.Duration {
+	if hz <= 0 {
+		panic("Hz must be positive")
+	}
+	seconds := 1.0 / hz
+	return time.Duration(seconds * float64(time.Second))
+}
